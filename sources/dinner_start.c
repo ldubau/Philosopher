16cb6ff4adcbcc208b45c1c15dd16dc1c7973945
ldubau <starline4.kr3w33@gmail.com>
2026-09-22 10:50:02 +0200

    add eat function

--- sources/dinner_start.c
+++ sources/dinner_start.c
@@ -3,22 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   dinner_start.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldubau <ldubau@student.42.fr>              +#+  +:+       +#+        */
+/*   By: leonpouet <leonpouet@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/21 11:50:02 by ldubau            #+#    #+#             */
-/*   Updated: 2026/09/21 17:56:00 by ldubau           ###   ########.fr       */
+/*   Updated: 2026/09/22 10:45:20 by leonpouet        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	eat(t_table *table)
+void	eat(t_philo *philo)
 {
-	pthread_mutex_lock(&);
-	pthread_mutex_lock(&);
+	pthread_mutex_t	*first;
+	pthread_mutex_t	*second;
 
-	pthread_mutex_unlock(&);
-	pthread_mutex_unlock(&);
+	if (philo->id % 2 == 1)
+	{
+	first = philo->right_fork;
+	second = philo->left_fork;
+	}
+	else
+	{
+	first = philo->left_fork;
+	second = philo->right_fork;
+	}
+
+	pthread_mutex_lock(first);
+	mtx_printf(philo, "has taken a fork");
+	pthread_mutex_lock(second);
+	mtx_printf(philo, "has taken a fork");
+
+	mtx_printf(philo, "is eating");
+	philo->nbr_meal++;
+
+	philo->last_meal = get_time_ms();
+	pthread_mutex_unlock(first);
+	pthread_mutex_unlock(second);
 }
 
 void	dinner_start(t_table *table)
